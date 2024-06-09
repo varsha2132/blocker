b9b03ec871f37c494226ecf69d2c677ada9ea73d
KotrikeAmruthaVarsha <145061730+varsha2132@users.noreply.github.com>
2024-06-10 01:13:14 +0530

    Create website_blocker.py

---
+++ website_blocker.py
@@ -0,0 +1,18 @@
+import time
+
+# Define the configuration
+host_path = r"C:\Windows\System32\drivers\etc\hosts"
+web_sites_list = ["www.facebook.com", "facebook.com"]
+redirect = "127.0.0.1"
+
+# Open the hosts file and read its contents
+with open(host_path, "r+") as file:
+    content = file.read()
+
+    # Check and add the websites to the hosts file if they are not already blocked
+    for website in web_sites_list:
+        if website not in content:
+            file.write(redirect + " " + website + "\n")
+
+# Sleep for 10 seconds
+time.sleep(10)
